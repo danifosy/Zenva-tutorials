caab72aac61313620abc5781d7f43e15baf796e8
danifosy <mail@daniha.de>
2021-10-14 15:08:43 +0200

    Follow for loop tutorial

--- JS_course/Loop/script.js
+++ JS_course/Loop/script.js
@@ -17,3 +17,23 @@ while (fuel > 0) {
 }
 
 console.log(distance, fuel);
+
+let years = 10;
+
+let population = 100;
+// population = population * 1.05
+
+// let i = 0;
+
+// while (i < 10) {
+//   population *= 1.05;
+//   i++;
+// }
+
+// console.log(population);
+
+for (let i = 0; i < 10; i++) {
+  population *= 1.05;
+}
+
+console.log(population);
