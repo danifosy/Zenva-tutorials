6660821341e1bbecec7580abc5c4a164fd26661a
danifosy <mail@daniha.de>
2021-10-21 09:51:44 +0200

    Add mock checkAnswer function

--- JS_Language_Game/app/script.js
+++ JS_Language_Game/app/script.js
@@ -1,3 +1,4 @@
+//object for question
 const question = {
   title: "gato",
   alternatives: ["dog", "cat", "bird", "fish"],
@@ -7,28 +8,46 @@ console.log(question);
 
 const app = {
   start: function () {
-    const alternatives = document.querySelectorAll(".alternatives");
+    //get alternatives
+    const alternatives = document.querySelectorAll(".alternative");
 
     alternatives.forEach(function (element, index) {
       element.addEventListener("click", function () {
+        //check correct answers
+        this.checkAnswer(index);
         console.log("check correct answer");
       });
     });
+    //show first question
     this.showQuestion(question);
   },
 
   showQuestion: function (question) {
-    const titleDiv = document.getElementById("title");
-    const alternatives = document.querySelectorAll(".alternatives");
+    //keep track of current question
+    this.currentQuestion = question;
 
+    //show question title
+    const titleDiv = document.getElementById("title");
     titleDiv.textContent = question.title;
+
+    //show alternatives
+    const alternatives = document.querySelectorAll(".alternative");
 
     alternatives.forEach(function (element, index) {
       element.textContent = question.alternatives[index];
     });
   },
+
+  checkAnswer: function (userSelected) {
+    if (this.currentQuestion.correctAnswer === userSelected) {
+      //correct
+    } else {
+      //not correct
+    }
+  },
 };
 
+// start app
 app.start();
 
 const btn = document.getElementById("btn");
